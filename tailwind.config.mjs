3e89509a8a6095165ef61a1c77d17ab5174eecd8
lichenaut <mt0n@tutanota.com>
2024-06-07 13:05:42 -0600

    favorites icon wip

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,6 +6,8 @@ export default {
       colors: {
         "custom-gray": "#262626",
         "custom-blue": "#d5e8eb",
+        "leaflet-gray": "#cccccc",
+        "leaflet-dark-gray": "#1e1e1e",
       },
     },
   },
