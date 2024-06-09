8bcd82a40c7f0dff37a41db0a32e5e666037440b
lichenaut <mt0n@tutanota.com>
2024-06-08 19:15:30 -0600

    time localization, tested

--- src/services/favorite_service.ts
+++ src/services/favorite_service.ts
@@ -1,11 +1,11 @@
 export function addToFavorites(code: string) {
-  let item = localStorage.getItem("favorites");
+  let item: string | null = localStorage.getItem("favorites");
   if (item === null) {
     localStorage.setItem("favorites", JSON.stringify([code]));
     return;
   }
 
-  let favorites = JSON.parse(item) || [];
+  let favorites: string[] = JSON.parse(item) || [];
   if (!favorites.includes(code)) {
     favorites.push(code);
     localStorage.setItem("favorites", JSON.stringify(favorites));
@@ -13,18 +13,18 @@ export function addToFavorites(code: string) {
 }
 
 export function removeFromFavorites(code: string) {
-  let item = localStorage.getItem("favorites");
+  let item: string | null = localStorage.getItem("favorites");
   if (item === null) {
     return;
   }
 
-  let favorites = JSON.parse(item) || [];
+  let favorites: string[] = JSON.parse(item) || [];
   favorites = favorites.filter((favorite: string) => favorite !== code);
   localStorage.setItem("favorites", JSON.stringify(favorites));
 }
 
 export function getFavorites(): string[] | null {
-  let item = localStorage.getItem("favorites");
+  let item: string | null = localStorage.getItem("favorites");
   if (item === null) {
     return null;
   }
